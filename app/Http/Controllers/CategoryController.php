f96440edd66c8c67350a5202f65b3ba7c421501b
Anindow172-15-1618 <anindow15-1618@diu.edu.bd>
2021-11-11 02:48:03 +0600

    initial commit

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Auth;
+use Illuminate\Support\Carbon;
+
 
 class CategoryController extends Controller
 {
     public function AllCat(){
-        return view('admin.category.index');
+        $categories =Category::all();
+        return view('admin.category.index' , compact('categories'));
     }
 
 
@@ -18,5 +23,20 @@ class CategoryController extends Controller
         [
             'category_name.required' => 'PLease Input Category Name',
         ]);
+
+    Category::insert([
+        'category_name' => $request->category_name,
+        'user_id' => Auth::user()->id,
+        'created_at'=> Carbon::now()
+    ]); 
+    /*
+    $category = new Category;
+    $category->category_name = $request->category_name;
+    $category->user_id = Auth::user()->id;
+    $category->save(); */
+    
+    return Redirect()->back()->with('success','Category Inserted Successfull');
+
+
     }
 }
