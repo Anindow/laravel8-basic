39e77ba8778d51dab916b347b8a3f7a1888b1e06
Anindow172-15-1618 <anindow15-1618@diu.edu.bd>
2021-12-06 18:49:16 +0600

    temp test

--- resources/views/admin/slider/create.blade.php
+++ resources/views/admin/slider/create.blade.php
@@ -4,15 +4,14 @@
 <div class="col-lg-12">
 		<div class="card card-default">
 			<div class="card-header card-header-border-bottom">
-				<h2>Basic Form Controls</h2>
+				<h2>Create Slider</h2>
 			</div>
 
     <div class="card-body">
         <form>
             <div class="form-group">
-                <label for="exampleFormControlInput1">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Email">
-                <span class="mt-2 d-block">We'll never share your email with anyone else.</span>
+                <label for="exampleFormControlInput1">Slider Title </label>
+                <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="Slider Title">
             </div>
 
            
@@ -22,7 +21,7 @@
 
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
             </div>
 
             <div class="form-group">
